e9622bdc2c7c757070d687e4eb42fa88d416b928
israel.ekpo@israelekpo.com <israel.ekpo@israelekpo.com@c74709b6-6212-11de-bb41-d1906ad598ae>
2009-06-26 05:44:12 +0000

    A    trunk/csv_parser-config.in A    trunk/Makefile.in A    trunk/include A    trunk/include/csv_parser A    trunk/include/csv_parser/csv_parser.hpp A    trunk/Doxyfile A    trunk/AUTHORS A    trunk/ChangeLog A    trunk/csv_parser.cpp A    trunk/ltmain.sh A    trunk/csv_parser.hpp A    trunk/README A    trunk/configure.ac AM   trunk/libtool A    trunk/INSTALL A    trunk/COPYING A    trunk/Makefile.am A    trunk/driver.cpp A    trunk/NEWS AM   trunk/install-sh
    Adding the intial set of files

---
+++ driver.cpp
@@ -0,0 +1,98 @@
+
+#include <csv_parser.hpp>
+
+/**
+ * Driver File
+ *
+ * This object is used to parse text documents that are delimited by some
+ * type of character. Some of the common ones use spaces, tabs, commas and semi-colons.
+ *
+ * This is a list of common characters encountered by this program
+ *
+ * This list is from http://asciitable.com/
+ *
+ * @li DEC is how it would be represented in decimal form (base 10)
+ * @li HEX is how it would be represented in hexadecimal format (base 16)
+ *
+ * @li	DEC	HEX		Character Name
+ * @li	0	0x00	null
+ * @li	9	0x09	horizontal tab
+ * @li	10	0x0A	line feed, new line
+ * @li	13	0x0D	carriage return
+ * @li	27	0x1B	escape
+ * @li	32	0x20	space
+ * @li	33	0x21	double quote
+ * @li	39	0x27	single quote
+ * @li	44	0x2C	comma
+ * @li	92	0x5C	backslash
+ *
+ * @author Israel Ekpo <perfectvista@israelekpo.com>
+ */
+
+int main(void)
+{
+	/*
+	const char * filename = "sample_feeds/orlando_sentinel.csv";
+	const char * filename = "sample_feeds/yacht_council.csv";
+	const char * filename = "input.csv";
+
+	const char * filename = "sample_feeds/orlando_sentinel.csv";
+	const char enclosure_char = '"';
+	const char field_terminator = ';';
+	const char line_terminator  = '\n';
+
+	const char * filename = "input.csv";
+	const char enclosure_char = '"';
+	const char field_terminator = ',';
+	const char line_terminator  = '\n';
+
+	const char * filename = "input2.csv";
+	const char field_terminator = ',';
+	const char line_terminator  = '\n';
+
+	const char * filename = "input3.csv";
+	const char field_terminator = ',';
+	const char line_terminator  = '\n';
+
+	const char * filename = "sample_feeds/yacht_council.csv";
+	const char enclosure_char = '"';
+	const char field_terminator = ',';
+	const char line_terminator  = '\n';
+	*/
+
+	const char * filename = "input4.csv";
+	const char field_terminator = ',';
+	const char line_terminator  = '\n';
+	const char enclosure_char   = '"';
+
+	csv_parser file_parser;
+
+	file_parser.set_skip_lines(0);
+
+	file_parser.init(filename);
+	file_parser.set_enclosed_char(enclosure_char, ENCLOSURE_OPTIONAL);
+	file_parser.set_field_term_char(field_terminator);
+	file_parser.set_line_term_char(line_terminator);
+
+	unsigned int row_count = 1U;
+
+	while(file_parser.has_more_rows())
+	{
+		unsigned int i = 0;
+
+		csv_row row = file_parser.get_row();
+
+		for (i = 0; i < row.size(); i++)
+		{
+			printf("COLUMN %02d : %s\n", i + 1U, row[i].c_str());
+		}
+
+		printf("====================================================================\n");
+		printf("END OF ROW %02d\n", row_count);
+		printf("====================================================================\n");
+
+		row_count++;
+	}
+
+	return 0;
+}
