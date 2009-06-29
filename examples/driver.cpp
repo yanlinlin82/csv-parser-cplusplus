794bad4375030d0d78252a4f3a84df1fd819bf5d
israel.ekpo@israelekpo.com <israel.ekpo@israelekpo.com@c74709b6-6212-11de-bb41-d1906ad598ae>
2009-06-29 01:21:24 +0000

    M    trunk/doc/html/globals_defs.html M    trunk/doc/html/main.html M    trunk/doc/html/globals_eval.html M    trunk/doc/html/classcsv__parser.html M    trunk/doc/html/globals_func.html M    trunk/doc/html/pages.html M    trunk/doc/html/csv__parser_8cpp.html M    trunk/doc/html/functions.html M    trunk/doc/html/annotated.html M    trunk/doc/html/functions_func.html M    trunk/doc/html/globals.html M    trunk/doc/html/globals_type.html M    trunk/doc/html/classcsv__parser-members.html M    trunk/doc/html/driver_8cpp.html M    trunk/doc/html/files.html M    trunk/doc/html/functions_vars.html M    trunk/doc/html/todo.html M    trunk/doc/html/csv__parser_8hpp.html M    trunk/doc/html/globals_enum.html M    trunk/doc/rtf/refman.rtf M    trunk/examples/driver.cpp
    Added more documentation to the example driver.cpp file

--- examples/driver.cpp
+++ examples/driver.cpp
@@ -2,14 +2,9 @@
 #include <csv_parser/csv_parser.hpp>
 
 /**
- * Driver File
+ * Example Usage of libcsv_parser++
  *
- * This object is used to parse text documents that are delimited by some
- * type of character. Some of the common ones use spaces, tabs, commas and semi-colons.
- *
- * This is a list of common characters encountered by this program
- *
- * This list is from http://asciitable.com/
+ * These are some of the characters you may use in this program
  *
  * @li DEC is how it would be represented in decimal form (base 10)
  * @li HEX is how it would be represented in hexadecimal format (base 16)
@@ -25,12 +20,42 @@
  * @li	39	0x27	single quote
  * @li	44	0x2C	comma
  * @li	92	0x5C	backslash
+ */
+
+/**
+ * Example Program - showing usage of the csv_parser class
+ *
+ * In this example, we include the csv_parser header file as <csv_parser/csv_parser.hpp>
+ *
+ * Then we declare the variables we are going to use in the program
+ *
+ * @li The name of the input file is "example_input.csv"
+ * @li The field terminator is the comma character
+ * @li The record terminator is the new line character 0x0A
+ * @li The field enclosure character is the double quote.
+ *
+ * In this example we will be parsing the document as the fields are optionally enclosed.
+ *
+ * The first record in the CSV file will be skipped.
+ *
+ * Please view the source code of this file more closely for details.
+ *
+ * @todo Add more examples using different parsing modes and different enclosure and line terminator characters.
+ * @toto Include an example where the filename, field_terminator char, line_terminator char, enclosure_char and other program variables are loaded from a file.
+ *
+ * @param int The number of arguments passed
+ * @param argv The array of arguements passed to the main function
+ * @return int The status of the program returned to the operating system upon termination.
+ *
+ * @see csv_parser
  *
  * @author Israel Ekpo <israel.ekpo@israelekpo.com>
  */
-
-int main(void)
+int main(int argc, char ** argv)
 {
+	// const char * filename = argv[1];
+
+	/* Declare the variables to be used */
 	const char * filename = "example_input.csv";
 	const char field_terminator = ',';
 	const char line_terminator  = '\n';
@@ -38,21 +63,30 @@ int main(void)
 
 	csv_parser file_parser;
 
+	/* Define how many records we're gonna skip. This could be used to skip the column definitions. */
 	file_parser.set_skip_lines(1);
 
+	/* Specify the file to parse */
 	file_parser.init(filename);
+
+	/* Here we tell the parser how to parse the file */
 	file_parser.set_enclosed_char(enclosure_char, ENCLOSURE_OPTIONAL);
+
 	file_parser.set_field_term_char(field_terminator);
+
 	file_parser.set_line_term_char(line_terminator);
 
 	unsigned int row_count = 1U;
 
+	/* Check to see if there are more records, then grab each row one at a time */
 	while(file_parser.has_more_rows())
 	{
 		unsigned int i = 0;
 
+		/* Get the record */
 		csv_row row = file_parser.get_row();
 
+		/* Print out each column in the row */
 		for (i = 0; i < row.size(); i++)
 		{
 			printf("COLUMN %02d : %s\n", i + 1U, row[i].c_str());
